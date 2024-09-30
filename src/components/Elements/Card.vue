ab153422593e2cf906704aa55223ca74f9aeebd8
Park Yong Ha <yonghapk@gmail.com>
2024-09-30 18:42:31 +0900

    API 호출 추가

--- src/components/Elements/Card.vue
+++ src/components/Elements/Card.vue
@@ -1,6 +1,17 @@
 <template>
-	<div>
-		{{ info.addr1 }}
+	<div class="card">
+		<div class="card__image"
+		     :style="{
+				backgroundImage : `url(${info.firstimage})`
+		     }"
+		>
+		</div>
+		<h3 class="card__title">
+			{{ info.title }}
+		</h3>
+		<p class="card__address">
+			{{ info.addr1 }}
+		</p>
 	</div>
 </template>
 
@@ -9,3 +20,23 @@ const props = defineProps({
 	info : Object
 })
 </script>
+
+<style lang="scss" scoped>
+.card {
+	cursor:pointer;
+	&__image {
+		padding-top:57.25%;
+		background-size:cover;
+		background-repeat:no-repeat;
+		background-position:50% 50%;
+	}
+	&__title {
+		margin:10px 0;
+		font-size:1rem;
+	}
+	&__address {
+		font-size:0.8rem;
+		word-break:keep-all;
+	}
+}
+</style>
