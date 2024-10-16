da0ca374af23a92661ce0db024bbc052138c47c3
Park Yong Ha <yonghapk@gmail.com>
2024-10-16 23:44:47 +0900

    클래스명 변경

--- src/components/forms/Pagination.vue
+++ src/components/forms/Pagination.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="pagination">
-    <button class="pagination__number">&lt;</button>
-    <button class="pagination__number">1</button>
-    <button class="pagination__number pagination__number--active">2</button>
-    <button class="pagination__number">3</button>
-    <button class="pagination__number">&gt;</button>
+    <button class="pagination__button">&lt;</button>
+    <button class="pagination__button">1</button>
+    <button class="pagination__button pagination__number--active">2</button>
+    <button class="pagination__button">3</button>
+    <button class="pagination__button">&gt;</button>
   </div>
 </template>
 
@@ -14,7 +14,7 @@
 .pagination {
   padding: 20px 0;
   text-align: center;
-  &__number {
+  &__button {
     display: inline-block;
     width: 24px;
     height: 24px;
