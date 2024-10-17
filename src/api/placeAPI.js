4c4f0597f115dba4ab5a52fbb23f6465b380eb33
Park Yong Ha <yonghapk@gmail.com>
2024-10-17 23:42:59 +0900

    ADD pg

--- src/api/placeAPI.js
+++ src/api/placeAPI.js
@@ -9,7 +9,7 @@ export default () => ({
       "http://apis.data.go.kr/B551011/KorService1/areaBasedList1?contentTypeId=12&serviceKey=hhPRU4TihqC7sGrFL7uNTmty4I7Hng2A57yNkCPaRsb%2BbnlxyetnLDADCFy%2FDh0KshzZmRBEyFO1VEMKNHeuPg%3D%3D&numOfRows=20&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json" +
       params;
     const result = await http.get(url);
-    return result.data.response.body.items.item;
+    return result.data.response.body;
   },
   getPlaceDetail(id) {
     return http.get(
--- src/components/Elements/List.vue
+++ src/components/Elements/List.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup>
-import { inject, onMounted, ref, watch } from "vue";
+import { inject, onMounted, ref, watch, reactive, computed } from "vue";
 import { storeToRefs } from "pinia";
 import { useCommonStore } from "@/store/index";
 import Card from "@/components/Elements/Card.vue";
@@ -20,13 +20,21 @@ const store = useCommonStore();
 const factories = inject("factories");
 const { currentArea, currentArrange } = storeToRefs(store);
 const list = ref(null);
+const pageData = reactive({
+  totalCount: 0,
+  numberPerPage: 20,
+  totalPage: 1,
+});
 
 const fetchData = async () => {
   store.isLoading = true;
-  list.value = await factories.place.getPlaceList(
+  const result = await factories.place.getPlaceList(
     store.currentArea,
     store.currentArrange
   );
+  list.value = result.items.item;
+  pageData.totalCount = result.totalCount;
+  pageData.totalPage = Math.ceil(pageData.totalCount / pageData.numberPerPage);
   store.isLoading = false;
 };
 
